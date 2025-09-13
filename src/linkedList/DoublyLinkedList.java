ffd8a403762316c284185955f8ae9f27baa8c437
Ishank307 <ishankkumar307@gmail.com>
2025-09-14 02:19:57 +0530

    dll

---
+++ src/linkedList/DoublyLinkedList.java
@@ -0,0 +1,58 @@
+package linkedList;
+
+
+public class DoublyLinkedList {
+
+    Node head;
+
+    public void insertFirst(int val) {
+        Node node=new Node(val);
+        node.next=head;
+        node.prev=null;
+        if(head!=null) {
+            head.prev=node;
+        }
+
+        head=node;
+
+    }
+
+    public void display() {
+
+        Node node =head;
+        Node last = null;
+        while(node!=null) {
+            System.out.print(node.val+"->");
+            last=node;
+            node=node.next;
+        }
+        System.out.println("END");
+
+        while (last!=null){
+            System.out.print(last.val+"->");
+            last=last.prev;
+        }
+        System.out.println("start");
+    }
+
+
+    private class Node{
+
+
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val) {
+            this.val=val;
+        }
+
+        public Node(int val, Node next, Node prev) {
+            super();
+            this.val = val;
+            this.next = next;
+            this.prev=prev;
+        }
+    }
+}
+
---
+++ src/linkedList/Main.java
@@ -0,0 +1,14 @@
+package linkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList list=new DoublyLinkedList();
+
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(10);
+        list.display();
+    }
+}
