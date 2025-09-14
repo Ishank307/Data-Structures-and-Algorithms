e2f0979ab7ec1f24005bb461711df4666ac3f2e1
Ishank307 <ishankkumar307@gmail.com>
2025-09-14 13:52:07 +0530

    new

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -3,15 +3,22 @@ package linkedList;
 public class Main {
 
     public static void main(String[] args) {
-        DoublyLinkedList list=new DoublyLinkedList();
+//        DoublyLinkedList list=new DoublyLinkedList();
+//
+//        list.insertFirst(2);
+//        list.insertFirst(3);
+//        list.insertFirst(5);
+//        list.insertFirst(10);
+//        list.insertLast(1000);
+//
+//        list.insert(10,34);
+//        list.display();
 
-        list.insertFirst(2);
-        list.insertFirst(3);
-        list.insertFirst(5);
-        list.insertFirst(10);
-        list.insertLast(1000);
-
-        list.insert(10,34);
+        CircularLinkedList list=new CircularLinkedList();
+        list.insert(23);
+        list.insert(42);
+        list.insert(43);
+        list.delete(42);
         list.display();
     }
 }
